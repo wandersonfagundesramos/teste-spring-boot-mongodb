6c38b3beb54270ba431b6f8a131afa710f118aaf
Wanderson Fagundes <wandersonfagundesramos@gmail.com>
2018-10-24 16:37:32 -0300

    Primeiro commit - projeto criado testemongo

---
+++ src/test/java/com/fagundeswanderson/testemongodb/TestemongodbApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fagundeswanderson.testemongodb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestemongodbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
